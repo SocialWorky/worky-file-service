e851518ce9c98164b9359cef32295856961dc11c
Carlos Abarca Vega <soporte@compuelec.cl>
2024-06-03 19:05:28 -0400

    Add file upload functionality with destination customization

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,49 @@ import { UploadController } from './upload/upload.controller';
 import { UploadService } from './upload/upload.service';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Module({
   imports: [
     MulterModule.register({
       storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, callback) => {
-          const userId = req.body.userId || 'unknown';
+        destination: (req, file, callback) => {
+          const { destination } = req.body;
+          const uploadPath = path.join(
+            __dirname,
+            '..',
+            'uploads',
+            destination || 'worky_default',
+          );
 
+          fs.mkdirSync(uploadPath, { recursive: true });
+          callback(null, uploadPath);
+        },
+        filename: (req, file, callback) => {
+          const { userId } = req.body;
+          if (!userId) {
+            return callback(new Error('userId es requerido'), null);
+          }
           const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
           const ext = extname(file.originalname);
           const filename = `${userId}-${timestamp}${ext}`;
-
           callback(null, filename);
         },
       }),
+      fileFilter: (req, file, callback) => {
+        const allowedMimeTypes = [
+          'image/jpeg',
+          'image/png',
+          'video/mp4',
+          'video/mpeg',
+        ];
+        if (allowedMimeTypes.includes(file.mimetype)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Tipo de archivo no permitido'), false);
+        }
+      },
     }),
   ],
   controllers: [AppController, UploadController],
--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -4,12 +4,14 @@ import {
   UploadedFiles,
   UseInterceptors,
   Body,
+  BadRequestException,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
 
 class UploadDto {
   userId: string;
+  destination: string;
 }
 
 @Controller('upload')
@@ -17,11 +19,16 @@ export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post()
-  @UseInterceptors(FilesInterceptor('files', 10)) // Permite subir hasta 10 archivos
+  @UseInterceptors(FilesInterceptor('files', 10))
   uploadFiles(
     @UploadedFiles() files: Express.MulterFile[],
     @Body() body: UploadDto,
   ) {
+    if (!files || files.length === 0) {
+      throw new BadRequestException(
+        'No se han subido archivos o los archivos no son del tipo permitido.',
+      );
+    }
     return this.uploadService.uploadFiles(files, body.userId);
   }
 }
