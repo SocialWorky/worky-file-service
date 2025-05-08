e6b2831bfc354e26aa78d9d18ec763cea8e28a9b
Carlos Abarca Vega <soporte@compuelec.cl>
2025-05-08 15:03:11 -0400

    ci: update Redis host configuration to use environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { BullAdapter } from '@bull-board/api/bullAdapter';
     AuthModule,
     BullModule.forRoot({
       redis: {
-        host: 'localhost',
+        host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
       },
     }),
